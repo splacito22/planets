f1104ff2481a69987a23cea10ead1c5c084ffeff
Stephany <136421961+splacito22@users.noreply.github.com>
2023-12-14 17:38:26 -0800

    Added logic to a method that accepts some content and adds it to the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,6 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const db = await initdb();
   const tx = db.transaction("jate", "readwrite");
@@ -24,7 +23,6 @@ export const putDb = async (content) => {
   console.log("Content added to the database");
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   const db = await initdb();
   const tx = db.transaction("jate", "readonly");
